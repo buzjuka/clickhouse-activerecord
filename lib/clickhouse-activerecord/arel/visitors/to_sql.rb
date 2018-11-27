c9e1fbae1a2bdfbacaf6c97c0f40596dceeace70
Kirill Grigorjev <buzjukagm@gmail.com>
2018-11-27 09:59:42 +0300

    override where clause

--- lib/clickhouse-activerecord/arel/visitors/to_sql.rb
+++ lib/clickhouse-activerecord/arel/visitors/to_sql.rb
@@ -25,6 +25,59 @@ module ClickhouseActiverecord
           collector.instance_variable_get(:@parts)[-1] = ','
           visit limit.to_i, collector
         end
+
+        def visit_Arel_Nodes_SelectCore o, collector
+          collector << "SELECT"
+
+          collector = maybe_visit o.top, collector
+
+          collector = maybe_visit o.set_quantifier, collector
+
+          unless o.projections.empty?
+            collector << SPACE
+            len = o.projections.length - 1
+            o.projections.each_with_index do |x, i|
+              collector = visit(x, collector)
+              collector << COMMA unless len == i
+            end
+          end
+
+          if o.source && !o.source.empty?
+            collector << " FROM "
+            collector = visit o.source, collector
+          end
+
+          unless o.wheres.empty?
+            collector << self.class::WHERE
+            len = o.wheres.length - 1
+            o.wheres.each_with_index do |x, i|
+              collector = visit(x, collector)
+              collector << AND unless len == i
+            end
+          end
+
+          unless o.groups.empty?
+            collector << GROUP_BY
+            len = o.groups.length - 1
+            o.groups.each_with_index do |x, i|
+              collector = visit(x, collector)
+              collector << COMMA unless len == i
+            end
+          end
+
+          collector = maybe_visit o.having, collector
+
+          unless o.windows.empty?
+            collector << WINDOW
+            len = o.windows.length - 1
+            o.windows.each_with_index do |x, i|
+              collector = visit(x, collector)
+              collector << COMMA unless len == i
+            end
+          end
+
+          collector
+        end
       end
     end
   end
